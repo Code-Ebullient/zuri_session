69c9d1b5ed92e836b579d772a188cd756fbab274
chassis <38101674+Code-Ebullient@users.noreply.github.com>
2022-06-11 23:51:36 +0100

    Update reset.php

--- php/reset.php
+++ php/reset.php
@@ -3,16 +3,21 @@ if(isset($_POST['submit'])){
     $email = trim($_POST['email']);// Defining email variable
     $newpassword = trim($_POST['password']);//Defining newpassword variable
 
-    resetPassword($email, $password);
+    resetPassword($email, $newpassword);
 }
 
-function resetPassword($email, $password){
-    //open file and check if the username exist inside
-    $file = fopen("../storage/users.csv", 'r');
-    if(strstr($email[$_POST] == ))
-
-    //if it does, replace the password
+function resetPassword($email, $newpassword){
+    //open file and 
+    $file = fopen("../storage/users.csv", 'r+'); //open file
+    $data = json_decode(file_get_contents($file, $this->storage)); //check if the email exist
+    if(preg_match_all("/[\._a-zA-Z0-9-]+@[\._a-zA-Z0-9-]+/i", $data, $matches)){ //if it does, replace the password
+        $newpassword[$email] = $newpassword;
+        file_put_contents($this->storage, json_decode($newpassword));
+        //var_dump ($newpassword);
+    } else {
+        echo "Email does not match";
+    }
+    fclose($file);
 }
-echo "HANDLE THIS PAGE";
 
 
